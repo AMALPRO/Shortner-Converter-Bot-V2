32d7b22db72649147f755a4b1feb3b7b01b709f2
AMALPRO <87407213+AMALPRO@users.noreply.github.com>
2023-08-02 23:27:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ OWNER_ID =  int(os.environ.get("OWNER_ID", "Owner Id"))
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
 ADMINS.append("1869495895")
 #  Optionnal variables
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "Logs Channels Id")) 
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001926199759")) 
 UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "Updates Channel User name Without @") # For Force Subscription
 BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "True") # true if forward should be avoided
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '') # image when someone hit /start # image when someone hit /start
